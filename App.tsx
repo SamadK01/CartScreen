1b7286bbf7f45a5697e343f1b6aa35289f45253b
Abdul Samad <samadkhalid01@gmail.com>
2024-05-07 09:43:58 +0500

    Cart Screen

---
+++ App.tsx
@@ -0,0 +1,235 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList, Image, SafeAreaView } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const appleImage = require('./src/images/candle.jpeg');
+const bananaImage = require('./src/images/nugg.jpeg');
+const cherryImage = require('./src/images/candle.jpeg');
+
+const products = [
+  { id: '1', name: 'Apple', price: 100, image: appleImage, discount: 15 },
+  { id: '2', name: 'Banana', price: 80, image: bananaImage, discount: 10 },
+  { id: '3', name: 'Cherry', price: 150, image: cherryImage },
+  { id: '4', name: 'Cherry', price: 170, image: cherryImage, discount: 20 }
+];
+
+export default function App() {
+  const [cart, setCart] = useState([]);
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  const addToCart = (product) => {
+    const productExists = cart.find(item => item.id === product.id);
+    if (productExists) {
+      setCart(cart.map(item => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item));
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (productId) => {
+    setCart(cart.filter(item => item.id !== productId));
+    setSelectedIds(selectedIds.filter(id => id !== productId));
+  };
+
+  const toggleSelection = (id) => {
+    if (selectedIds.includes(id)) {
+      setSelectedIds(selectedIds.filter(item => item !== id));
+    } else {
+      setSelectedIds([...selectedIds, id]);
+    }
+  };
+
+  const deleteSelectedItems = () => {
+    setCart(cart.filter(item => !selectedIds.includes(item.id)));
+    setSelectedIds([]);
+  };
+
+  const getTotalPrice = () => {
+    return cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  };
+
+  const totalItemsInCart = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Shopping Cart ({totalItemsInCart})</Text>
+        <TouchableOpacity onPress={deleteSelectedItems} style={styles.deleteButton}>
+          <MaterialCommunityIcons name="delete" size={25} color="white" />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <TouchableOpacity
+              onPress={() => toggleSelection(item.id)}
+              style={[
+                styles.checkboxContainer,
+                { backgroundColor: selectedIds.includes(item.id) ? '#FF612F' : 'white' }
+              ]}
+            >
+              <Text style={[
+                selectedIds.includes(item.id) ? styles.checkboxChecked : styles.checkboxUnchecked
+              ]}>
+                ✓
+              </Text>
+            </TouchableOpacity>
+            <Image source={item.image} style={styles.image} />
+            <View style={styles.infoContainer}>
+              <View>
+                <Text style={styles.itemName}>{item.name}</Text>
+                <Text style={styles.itemPrice}>${item.price}</Text>
+                {item.discount && <Text style={styles.discountText}>{item.discount}% Discount</Text>}
+              </View>
+              <View style={styles.quantityContainer}>
+                <TouchableOpacity onPress={() => removeFromCart(item.id)} style={styles.quantityButton}>
+                  <Text style={styles.buttonText}>-</Text>
+                </TouchableOpacity>
+                <Text style={styles.quantityText}>{cart.find(cartItem => cartItem.id === item.id)?.quantity || 0}</Text>
+                <TouchableOpacity onPress={() => addToCart(item)} style={styles.quantityButton}>
+                  <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+      <View style={styles.footer}>
+        <Text style={styles.total}>Total: ${getTotalPrice()}</Text>
+        <TouchableOpacity style={styles.checkoutButton}>
+          <Text style={styles.checkoutButtonText}>Checkout ({selectedIds.length})</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop:20,
+    paddingHorizontal: 20,
+    backgroundColor: '#f0f0f0'
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 15,
+    backgroundColor: '#FFD4C7',
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 10
+  },
+  headerTitle: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  deleteButton: {
+    padding: 10,
+    borderRadius: 5
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    marginVertical: 5,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 4
+  },
+  checkboxContainer: {
+    marginRight: 15,
+    width: 28,
+    height: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: '#FF612F',
+    borderRadius: 14,
+    backgroundColor: 'white'
+  },
+  checkboxChecked: {
+    color: 'white',
+    fontSize: 14,
+    lineHeight: 20
+  },
+  checkboxUnchecked: {
+    fontSize: 14,
+    color: 'transparent',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 20
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  itemName: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#0E1722',
+  },
+  itemPrice: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#0E1722',
+  },
+  discountText: {
+    fontSize: 12,
+    fontWeight: '400',
+    color: '#FF6347',
+    marginTop: 4
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  quantityButton: {
+    backgroundColor: '#F6F6F6',
+    borderRadius: 30,
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16
+  },
+  quantityText: {
+    marginHorizontal: 10,
+    fontSize: 16
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 10
+  },
+  total: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  checkoutButton: {
+    backgroundColor: '#FF612F',
+    padding: 10,
+    borderRadius: 5
+  },
+  checkoutButtonText: {
+    color: 'white',
+    fontSize: 16
+  }
+});
